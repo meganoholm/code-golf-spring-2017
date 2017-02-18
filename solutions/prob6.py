c89686d813e908070419381974fd968d7f66267c
Megan Holmberg <meganoholm@gmail.com>
2017-02-18 13:53:54 -0600

    worked on 6

---
+++ solutions/prob6.py
@@ -0,0 +1,6 @@
+f=open('/Users/megan/Code/code-golf-spring-2017/inputs/6.txt','r')
+o = open('/Users/megan/Code/code-golf-spring-2017/answers/6.txt', 'w')
+for line in f:
+    res = ''
+    sep = line.split()
+    print sep
